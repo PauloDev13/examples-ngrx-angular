1a082d40ceffe6b3db0a2f6b4d7d32650b1a0413
PauloDev13 <devpgm2020@gmail.com>
2021-10-17 20:22:54 -0300

    Create Reactive Form for adding Post and apply validations using child routing

--- src/app/post/post-routing.module.ts
+++ src/app/post/post-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AddPostComponent } from './add-post/add-post.component';
 import { PostListComponent } from './post-list/post-list.component';
 
 const routes: Routes = [
   {
     path: 'posts',
     component: PostListComponent,
+    children: [
+      {
+        path: 'add',
+        component: AddPostComponent,
+      },
+    ],
   },
 ];
 
--- src/app/post/post.module.ts
+++ src/app/post/post.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { AddPostComponent } from './add-post/add-post.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { PostRoutingModule } from './post-routing.module';
 
 @NgModule({
-  declarations: [PostListComponent],
-  imports: [CommonModule, PostRoutingModule],
+  declarations: [PostListComponent, AddPostComponent],
+  imports: [CommonModule, PostRoutingModule, ReactiveFormsModule],
 })
 export class PostModule {}
