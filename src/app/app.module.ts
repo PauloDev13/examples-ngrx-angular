5a4423546108bfe019517f8fd2e1d98431517ca8
PauloDev13 <devpgm2020@gmail.com>
2021-10-17 16:39:34 -0300

    Implement Routing for the state management Ngrx application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterModule } from './counter/counter.module';
+import { HomeComponent } from './home/home.component';
+import { PostModule } from './post/post.module';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, CounterModule],
+  declarations: [AppComponent, HeaderComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, CounterModule, PostModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/post/post-routing.module.ts
+++ src/app/post/post-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { CounterComponent } from './counter/counter.component';
+import { PostListComponent } from './post-list/post-list.component';
 
 const routes: Routes = [
   {
-    path: 'counter',
-    component: CounterComponent,
+    path: 'posts',
+    component: PostListComponent,
   },
 ];
 
@@ -14,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class CounterRoutingModule {}
+export class PostRoutingModule {}
